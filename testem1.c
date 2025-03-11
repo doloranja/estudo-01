33240165af7d56f65193868a7f3d6709fef5d57f
doloranja <lucasfreitasdolo@gmail.com>
2025-03-11 09:41:07 -0300

    Opção de saida
    adicionada opção de saída para o usuário

--- testem1.c
+++ testem1.c
@@ -142,7 +142,8 @@ int main ()
 	printf ("Escolha a opção desejada do menu:\n\n");
 	printf ("\t1 - Registrar Nomes\n");
 	printf ("\t2 - Consultar Nomes\n");
-	printf ("\t3 - Deletar Nomes\n\n");
+	printf ("\t3 - Deletar Nomes\n");
+	printf ("\t4 - Sair do sistema\n\n");
 	printf ("opção: "); //fim do menu
 
 
@@ -162,6 +163,11 @@ int main ()
 		
 	case 3:
 	deletar();
+	break;
+	
+	case 4:
+	printf("Obrigado, Volte Sempre");
+	return 0;
 	break;
 		
 	default:
